329c14fb0f31b6ae1cc3783ec2cef432344991b4
iolynx <santaclawsers@gmail.com>
2024-07-12 03:38:44 +0530

    split particle class into a new file

---
+++ particle.py
@@ -0,0 +1,43 @@
+import math
+import pygame
+from pygame import screen
+
+class Particle:
+    def __init__(self, x, y, xvel, yvel, mass, radius, colour):
+        self.x = x
+        self.y = y
+        self.xVel = xvel
+        self.yVel = yvel
+        self.radius = radius
+        self.colour = colour
+        self.mass = mass
+
+    def draw(self):
+        pygame.draw.circle(screen, self.colour,
+                           (self.x, self.y), self.radius, self.radius)
+
+    def drawTrail(self, surface):
+        pygame.draw.circle(surface, self.colour,
+                           (self.x, self.y), self.radius//2, self.radius//2)
+
+    def updatePos(self):
+        self.x += self.xVel
+        self.y += self.yVel
+
+    '''def updateVel(self, xForce, yForce):
+		self.xVel = xForce / self.mass
+		self.yVel = yForce / self.mass'''
+
+    def updateForces(self, p):
+
+        diffx = p.x - self.x
+        diffy = p.y - self.y
+        dist = math.sqrt(diffx**2 + diffy**2)
+        fNet = (G * p.mass * self.mass)/(dist**2)
+
+        theta = math.atan2(diffy, diffx)
+
+        acc = fNet / self.mass
+
+        self.xVel += (acc * math.cos(theta))
+        self.yVel += (acc * math.sin(theta))
